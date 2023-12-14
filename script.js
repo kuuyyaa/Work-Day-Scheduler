8a3dd4542b6557079f47bed585b6c96deb70acad
kuuyyaa <146645814+kuuyyaa@users.noreply.github.com>
2023-12-15 02:37:40 +0400

    create timeblocks

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
     // display hours of current day
-    $('#current-day').text(dayjs().format('dddd, MMMM D, YYY'));
+    $('#currentDay').text(dayjs().format('dddd, MMMM D, YYY'));
 
     // create timeblocks
-    
+    for (let i = 0; i < 24; i++) {
+        let timeblock = $('<div>').addClass('timeblock');
+        let hour = $('<span>').text(`${hour}:00`);
+        let input = $('<input>').attr('type', 'text');
+        let saveButton = $('<button>').addClass('save-button').text('Save');
+
+        // timeblock.append(hour, input, saveButton);
+        // $('#timeblocks').append(timeblock);
+        
+    }
 
 });
